3344c5457075807eae530d9c6c57739eb8e14b80
Daniel Barclay <daniel@urbanintel.io>
2019-04-13 21:32:37 +0100

    Dynamically merge PDFs using command line, selecting files and page ranges to be merged

--- pdf.py
+++ pdf.py
@@ -1,43 +1,62 @@
 import PyPDF2
-
-output_file = open('merged.pdf', 'wb')
-pdf_writer = PyPDF2.PdfFileWriter()
+# welcome message
+print("Welcome to PDF Merge, a handy python script to help you merge PDFs for free")
 
 
 class PDF:
-    def __init__(self, name):
-        self.name = name
-        self.path = './pdfs/' + name
-        self.pages = 0
+    def __init__(self):
+        self.path = None
+        self.pages = None
         self.pdf_reader = None
+        self.page_range = None
 
     def open(self):
         return open(self.path, 'rb')
 
     def read(self):
         self.pdf_reader = PyPDF2.PdfFileReader(self.open())
-
-    def add_pages(self):
         self.pages = self.pdf_reader.getNumPages()
 
-        for page in range(self.pages):
+    def add_pages(self, page_range):
+        for page in range((page_range[0]-1), page_range[1]):
             pdf_writer.addPage(self.pdf_reader.getPage(page))
 
     def close(self):
         self.open().close()
 
     def merge(self):
+        self.get_file_name()
         self.open()
         self.read()
-        self.add_pages()
+        self.display_pages()
+        self.set_page_range()
+        self.add_pages(self.page_range)
         self.close()
+
+    def display_pages(self):
+        print("{} has {} pages".format(self.path, self.pages))
+
+    def set_page_range(self):
+        first_page = int(input("Number of first page of PDF to be merged: "))
+        last_page = int(input("Number of last page of PDF to be merged: "))
+        self.page_range = (first_page, last_page)
+
+    def get_file_name(self):
+        self.path = input('Please enter the name of the PDF file you wish to merge: ')
 
 
 # instantiated classes
-pdf_two = PDF('three-pages.pdf')
+pdf_writer = PyPDF2.PdfFileWriter()
+
+pdf_two = PDF()
 pdf_two.merge()
 
-pdf_one = PDF('two-pages.pdf')
+pdf_one = PDF()
 pdf_one.merge()
 
+# set output file settings
+output_file_name = input('Save new file as : ')
+output_file = open(output_file_name, 'wb')
+
+# write merged pdf file
 pdf_writer.write(output_file)
