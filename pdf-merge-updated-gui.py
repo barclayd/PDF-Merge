95effe5171646adc8d9993eb7fcd3823c6e37a35
Daniel Barclay <barclaysd@me.com>
2019-04-15 14:24:43 +0100

    Functionality added to new GUI to allow page range to be persisted when changing between PDFs

--- pdf-merge-updated-gui.py
+++ pdf-merge-updated-gui.py
@@ -6,26 +6,31 @@ root.title('PDF Merge')
 # PDFs
 documents = []
 documents_length = IntVar()
-test = None
-display = None
+filename = StringVar()
+page_count = StringVar()
+page_start = StringVar()
+page_end = StringVar()
 
 
 class PDF:
-    def __init__(self):
-        self.file = None
-        self.filename = StringVar()
-        self.pages = IntVar()
+    def __init__(self, file):
+        self.file = file
+        self.filename = file.split('/')[-1]
+        self.pages = self.set_pages()
         self.pdf_reader = None
-        self.page_range = [IntVar(), IntVar()]
+        self.start = 1
+        self.end = self.pages
 
     def open(self):
         return open(self.file, 'rb')
 
     def read(self):
         self.pdf_reader = PyPDF2.PdfFileReader(self.open())
-        self.pages.set(self.pdf_reader.getNumPages())
-        self.page_range[0].set(1)
-        self.page_range[1].set(int(self.pages.get()))
+        self.close()
+
+    def set_pages(self):
+        self.read()
+        return self.pdf_reader.getNumPages()
 
     def add_pages(self, page_range):
         for page in range((int(page_range[0].get())-1), int(page_range[1].get())):
@@ -38,14 +43,8 @@ class PDF:
         self.get_file_name()
         self.open()
         self.read()
-        self.set_page_range()
-        self.add_pages(self.page_range)
+        self.add_pages([self.start, self.end])
         self.close()
-
-    def set_page_range(self):
-        first_page = int(input("Number of first page of PDF to be merged: "))
-        last_page = int(input("Number of last page of PDF to be merged: "))
-        self.page_range = (first_page, last_page)
 
     def get_file_name(self):
         self.file = input('Please enter the name of the PDF file you wish to merge: ')
@@ -56,14 +55,12 @@ class PDF:
         self.read()
 
     def add_to_merged_pdf(self):
-        self.add_pages(self.page_range)
+        self.add_pages([self.start, self.end])
         self.close()
 
 
 # instantiated classes
 pdf_writer = PyPDF2.PdfFileWriter()
-pdf_two = PDF()
-pdf_one = PDF()
 
 
 # functions
@@ -80,28 +77,70 @@ def merge_documents():
 
 
 def add_new_pdf():
-    documents.append(PDF())
-    documents_length.set(len(documents))
+    file = askopenfilename(filetypes=(("PDF File", "*.pdf"), ('All Files', '*.*')))
+    new_pdf = PDF(file)
+    new_pdf.read()
+    documents.append(new_pdf)
+    # update listbox
+    listbox.insert(END, new_pdf.filename)
+
+
+def remove_pdf():
+    documents.pop(get_selected_pdf())
+    listbox.delete(ANCHOR)
+    clear_display()
+
+
+def display_pdf(*args):
+    index = int(listbox.curselection()[0])
+    value = listbox.get(index)
+    print(value)
+    filename.set(documents[index].filename)
+    page_count.set(documents[index].pages)
+    page_start.set(documents[index].start)
+    page_end.set(documents[index].end)
+
+
+def clear_display():
+    global filename, page_count, page_start, page_end
+    filename = StringVar()
+    page_count = StringVar()
+    page_start = StringVar()
+    page_end = StringVar()
+
+
+def get_selected_pdf():
+    return int(listbox.curselection()[0])
+
+
+def set_page_start(*args):
+    index = int(listbox.curselection()[0])
+    documents[index].start = int(page_start.get())
+
+
+def set_page_end(*args):
+    index = int(listbox.curselection()[0])
+    documents[index].end = int(page_end.get())
 
 
 # GUI
 Label(root, text='Combine and Merge PDFs').grid(row=0, column=0, columnspan=4)
 Button(root, text='Add PDF', command=add_new_pdf).grid(row=2, column=0)
-Button(root, text='Remove PDF', command=add_new_pdf).grid(row=3, column=0)
+Button(root, text='Remove PDF', command=remove_pdf).grid(row=3, column=0)
 
 listbox = Listbox(root)
-listbox.bind('<<ListboxSelect>>', display)
+listbox.bind('<<ListboxSelect>>', display_pdf)
 listbox.grid(row=1, rowspan=4, column=1)
 
 Label(root, text='File: ').grid(row=1, column=2)
-Label(root, textvariable=test, width=20).grid(row=1, column=3, sticky=(N, S, E, W))
+Label(root, textvariable=filename, width=20).grid(row=1, column=3, sticky=(N, S, E, W))
 Label(root, text='Pages: ').grid(row=2, column=2)
-Label(root, textvariable=test, width=20).grid(row=2, column=3, sticky=(N, S, E, W))
+Label(root, textvariable=page_count, width=20).grid(row=2, column=3, sticky=(N, S, E, W))
 Label(root, text='Start: ').grid(row=3, column=2)
-entry_1 = Entry(root, textvariable=test, width=3)
+entry_1 = Entry(root, textvariable=page_start, width=3)
 entry_1.grid(row=3, column=3)
 Label(root, text='End: ').grid(row=4, column=2)
-entry_2 = Entry(root, textvariable=test, width=3)
+entry_2 = Entry(root, textvariable=page_end, width=3)
 entry_2.grid(row=4, column=3)
 
 Button(root, text='Save and Merge PDF', command=merge_documents, width=10).grid(row=5, column=0, columnspan=4)
@@ -109,5 +148,9 @@ Button(root, text='Save and Merge PDF', command=merge_documents, width=10).grid(
 # style GUI
 for child in root.winfo_children():
     child.grid_configure(padx=10, pady=10)
+
+# trace variable changes
+page_start.trace('w', set_page_start)
+page_end.trace('w', set_page_end)
 
 root.mainloop()
