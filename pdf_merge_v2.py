84b67353f3a8e65d4b8fd27d029d9ab8247d1958
Daniel Barclay <barclaysd@me.com>
2019-04-15 15:00:03 +0100

    Refactored version 2 to use a shared pdf_merge library and added merge and save functionality

--- pdf_merge_v2.py
+++ pdf_merge_v2.py
@@ -1,66 +1,5 @@
-import PyPDF2
-from tkinter.filedialog import *
-root = Tk()
+from pdf_merge import *
 root.title('PDF Merge')
-
-# PDFs
-documents = []
-documents_length = IntVar()
-filename = StringVar()
-page_count = StringVar()
-page_start = StringVar()
-page_end = StringVar()
-
-
-class PDF:
-    def __init__(self, file):
-        self.file = file
-        self.filename = file.split('/')[-1]
-        self.pages = self.set_pages()
-        self.pdf_reader = None
-        self.start = 1
-        self.end = self.pages
-
-    def open(self):
-        return open(self.file, 'rb')
-
-    def read(self):
-        self.pdf_reader = PyPDF2.PdfFileReader(self.open())
-        self.close()
-
-    def set_pages(self):
-        self.read()
-        return self.pdf_reader.getNumPages()
-
-    def add_pages(self, page_range):
-        for page in range((int(page_range[0].get())-1), int(page_range[1].get())):
-            pdf_writer.addPage(self.pdf_reader.getPage(page))
-
-    def close(self):
-        self.open().close()
-
-    def merge(self):
-        self.get_file_name()
-        self.open()
-        self.read()
-        self.add_pages([self.start, self.end])
-        self.close()
-
-    def get_file_name(self):
-        self.file = input('Please enter the name of the PDF file you wish to merge: ')
-
-    def load_file(self):
-        self.file = askopenfilename(filetypes=(("PDF File", "*.pdf"), ('All Files', '*.*')))
-        self.filename.set(self.file.split('/')[-1])
-        self.read()
-
-    def add_to_merged_pdf(self):
-        self.add_pages([self.start, self.end])
-        self.close()
-
-
-# instantiated classes
-pdf_writer = PyPDF2.PdfFileWriter()
 
 
 # functions
@@ -78,11 +17,11 @@ def merge_documents():
 
 def add_new_pdf():
     file = askopenfilename(filetypes=(("PDF File", "*.pdf"), ('All Files', '*.*')))
-    new_pdf = PDF(file)
-    new_pdf.read()
-    documents.append(new_pdf)
+    pdf = PDF(file)
+    pdf.read()
+    documents.append(pdf)
     # update listbox
-    listbox.insert(END, new_pdf.filename)
+    listbox.insert(END, pdf.filename)
 
 
 def remove_pdf():
@@ -93,8 +32,6 @@ def remove_pdf():
 
 def display_pdf(*args):
     index = int(listbox.curselection()[0])
-    value = listbox.get(index)
-    print(value)
     filename.set(documents[index].filename)
     page_count.set(documents[index].pages)
     page_start.set(documents[index].start)
